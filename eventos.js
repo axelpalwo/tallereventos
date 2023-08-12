afd8f5a76c5fb98c8fed0ced8e367a5808ffe3cf
AxelPalWO <140984664+AxelPalWO@users.noreply.github.com>
2023-08-11 21:36:59 -0300

    9 puntos terminados en la tarea

---
+++ eventos.js
@@ -0,0 +1,8 @@
+const divbutton = document.getElementById("divbtn");
+
+function saludar(){
+    alert("Hola");
+}
+divbutton.addEventListener("click",function(e){
+    alert("Hola! soy el div")
+})
